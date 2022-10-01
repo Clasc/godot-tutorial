f40a488e5517c79bf5f20f1a842b25cc36b4a05b
Christian Lascsak <christian.lascsak7@gmail.com>
2022-10-01 16:43:09 +0200

    init

---
+++ Main.gd
@@ -0,0 +1,66 @@
+extends Node
+
+
+export(PackedScene) var mob_scene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+func _process(delta):
+	if Input.is_action_pressed("key_exit"):
+		get_tree().quit()
+		
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+func on_player_hit():
+	game_over()
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instance()
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
+
+
+func on_score_timer_timeout():
+	score+=1
+	$HUD.update_score(score)
+
+func on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func start_game():
+	score = 0
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
